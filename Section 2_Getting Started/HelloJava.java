acf84118734ea6360ee2d76a5f7366135665f3a9
Lucifersfier <nssrivas@gmail.com>
2024-01-24 00:10:08 +0530

    First commit till variables

---
+++ Section 2_Getting Started/HelloJava.java
@@ -0,0 +1,36 @@
+public class HelloJava {
+    // Main is the entry point of a java application This is where java starts running code.
+    public static void main(String[] args) {
+        System.out.println("Hello World!");  // Print "Hello World!" to the console
+    }
+}
+
+/*when you execute your code, java looks for the entry point, it looks for main tries to find it 
+and once it does, its going to run the code inside */  
+
+// ************* TERMINAL COMMANDS ******************//
+/* javac : compiles javacode into byte code 
+javac <file_name>.java
+java <file_name>  */
+
+/*******************       CHEATSHEET      *******************/    
+/* 
+This cheat sheet contains the most important takeaways that lead up to section one.
+
+1. Getting Started
+
+class: contains all of your code.
+main(): entry point of your app.
+javac <file-name>.java: compiles your code.
+java <file-name>: runs the compiled code.
+
+Coding Pitfalls
+None yet.
+
+Good coding habits
+The name of a class must be CamelCase.
+
+Tips and tricks
+Use the up key to run previous terminal commands.
+Use the tab key to autocomplet
+ */
---
+++ Section 3_Challenge/Yourinitials.java
@@ -0,0 +1,30 @@
+public class Yourinitials {
+    public static void main(String[] args) {
+        System.out.println(" SSS     A");
+        System.out.println("S   S   A A");
+        System.out.println("S      A   A");
+        System.out.println(" SSS   AAAAA");
+        System.out.println("    S  A   A");
+        System.out.println("S   S  A   A");
+        System.out.println(" SSS   A   A");
+    }
+}
+/*
+public: It means that the main method can be accessed from anywhere.
+        It is necessary for the JVM to locate and execute the method.
+
+static: The main method is associated with the class, not with any instance of the class.
+        This keyword allows the method to be called without creating an instance of the
+        class.
+
+void: It indicates that the main method doesn't return any value.
+
+main: This is the name of the method. It is a special name recognized by the JVM
+      as the entry point for the program.
+
+String[] args: This parameter allows you to pass command-line arguments to your
+               Java program. The args parameter is an array of strings that contains
+               any command-line arguments passed to the program. 
+               You can use these arguments to customize the behavior of your program.
+ 
+*/
